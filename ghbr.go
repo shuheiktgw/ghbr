61d3b67e5ed1cff894830fffdd796d583e4b4b75
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-08-12 20:09:51 +0900

    Add GetLatestRelease method

--- ghbr.go
+++ ghbr.go
@@ -5,13 +5,61 @@ import (
 	"os"
 	"net/http"
 	"github.com/pkg/errors"
+	"github.com/google/go-github/github"
+	"strings"
+	"crypto/sha256"
 )
+
+const MacRelease = "darwin_amd64"
 
 // GHBR define functions for Homebrew Formula
 type GHBR struct {
 	GitHub GitHub
 
 	outStream io.Writer
+}
+
+// LatestRelease contains latest release info
+type LatestRelease struct {
+	tag, hash string
+}
+
+// GetLatestRelease returns the latest release tag and calculates its checksum
+func (g *GHBR) GetLatestRelease(repo string) (*LatestRelease, error) {
+	if len(repo) == 0 {
+		return nil, errors.New("missing GitHub repository")
+	}
+
+	// Get latest release of the repository
+	release, err := g.GitHub.GetLatestRelease(repo)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract tag
+	tag := *release.TagName
+
+	// Get Mac release asset browser URL
+	url, err := findMacReleaseURL(release)
+
+	// Download the release asset
+	path := "darwin_amd64.zip"
+	err = g.DownloadFile(path, url)
+	defer os.Remove(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Calculate hash
+	hash, err := calculateSha256(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &LatestRelease{tag: tag, hash: hash}, nil
 }
 
 // DownloadFile downloads a file from the url and save it to the path
@@ -52,6 +100,32 @@ func (g *GHBR) DownloadFile(path, url string) error {
 	}
 
 	return nil
+}
+
+func findMacReleaseURL(release *github.RepositoryRelease) (string, error) {
+	for _, a := range release.Assets {
+		if strings.Contains(*a.Name, MacRelease) {
+			return *a.BrowserDownloadURL, nil
+		}
+	}
+
+	return "", errors.Errorf("could not find assets named %s", MacRelease)
+}
+
+func calculateSha256(path string) (string, error) {
+	sha := sha256.New()
+
+	f, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	if _, err :=  io.Copy(sha, f); err != nil {
+		return "", err
+	}
+
+	return string(sha.Sum(nil)), nil
 }
 
 
--- ghbr_test.go
+++ ghbr_test.go
@@ -7,7 +7,40 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/shuheiktgw/ghbr/mocks"
+	"github.com/google/go-github/github"
 )
+
+func TestGHBR_GetLatestRelease_Success(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockGitHub := mocks.NewMockGitHub(mockCtrl)
+	mockAssets := []github.ReleaseAsset{
+		{
+			Name:github.String("gemer_v0.0.1_darwin_386.zip"),
+			BrowserDownloadURL: github.String("https://github.com/shuheiktgw/gemer/releases/download/0.0.1/gemer_v0.0.1_darwin_386.zip")},
+		{
+			Name:github.String("gemer_v0.0.1_darwin_amd64.zip"),
+			BrowserDownloadURL: github.String("https://github.com/shuheiktgw/gemer/releases/download/0.0.1/gemer_v0.0.1_darwin_amd64.zip")},
+	}
+	mockRelease := github.RepositoryRelease{
+		TagName: github.String("v0.0.1"),
+		Assets: mockAssets,
+	}
+	mockGitHub.EXPECT().GetLatestRelease("testRepo").Return(&mockRelease, nil).Times(1)
+
+	ghbr := GHBR{GitHub: mockGitHub, outStream: ioutil.Discard}
+
+	lr, err := ghbr.GetLatestRelease("testRepo")
+
+	if err != nil {
+		t.Fatalf("GetLatestRelease: unexpected error occured: %s", err)
+	}
+
+	if got, want := lr.tag, "v0.0.1"; got != want {
+		t.Fatalf("GetLatestRelease: wrong tag name is returned: got %s, want %s", got, want)
+	}
+}
 
 func TestGHBR_DownloadFile_Success(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
