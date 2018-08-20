05d94198ad10cbd61566ef2bb0f97c3b21bcdc79
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-08-20 09:43:16 +0900

    Add build environment

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eu
+
+# Extract value of Version const from version.go
+VERSION=`grep 'Version =' version/version.go | sed -E 's/.*"(.+)"$$/\1/'`
+
+# Path to built files
+FILES=./pkg/dist/v${VERSION}
+
+goxz -pv=v${VERSION} -arch=386,amd64 -d=${FILES}
+ghr -t ${GHR_GITHUB_TOKEN} --replace v${VERSION} ${FILES}
